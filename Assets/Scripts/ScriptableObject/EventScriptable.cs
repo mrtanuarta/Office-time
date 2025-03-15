d005b481806d17e88feb721e721806ac25f3f626
WD40t <150331419+WD40t@users.noreply.github.com>
2025-03-16 02:17:42 +0800

    Updated the event script to show left first, usually left signals yes

--- Assets/Scripts/ScriptableObject/EventScriptable.cs
+++ Assets/Scripts/ScriptableObject/EventScriptable.cs
@@ -7,13 +7,6 @@ public class EventScriptable : ScriptableObject
     public Sprite eventImage;
     public string eventName;
     public string eventDescription;
-    [Header("Swipe Right")]
-    public string rightDescription;
-    public float rightSanity;
-    public float rightReputation;
-    public float rightMoney;
-    public float rightWorkdone;
-    public int rightTimeSpent;
     [Header("Swipe Left")]
     public string leftDescription;
     public float leftSanity;
@@ -21,6 +14,13 @@ public class EventScriptable : ScriptableObject
     public float leftMoney;
     public float leftWorkdone;
     public int leftTimeSpent;
+    [Header("Swipe Right")]
+    public string rightDescription;
+    public float rightSanity;
+    public float rightReputation;
+    public float rightMoney;
+    public float rightWorkdone;
+    public int rightTimeSpent;
     [Header("Spawn Related")]
     public float weight;
 }
