4df0e6f95e5a2cef3eb4dc4d0a72a9a27df0a5a1
mrtanuarta <rickytanuarta123@gmail.com>
2025-03-16 15:44:50 +0800

    card manager

--- Assets/Scripts/CardManager.cs
+++ Assets/Scripts/CardManager.cs
@@ -2,5 +2,30 @@ using UnityEngine;
 
 public class CardManager : MonoBehaviour
 {
+    public static CardManager Instance { get; private set; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
     public EventScriptable[] cards;
+    public EventScriptable CurrCard;
+
+
+    void start()
+    {
+        //make an algorithm to pick currcard based on the list of cards based on the weight
+    }
+    void update()
+    {
+        //if a card is swiped left or right pick a new card
+    }
 }
